2ef6e1a45abe681248aa5927846f21f4689ae1b8
Pushpendra Pal <pushpendrapal9516@gmail.com>
2022-01-30 07:50:46 +0530

    6th video completed

---
+++ src/Understanding export/module1.mjs
@@ -0,0 +1,5 @@
+import uio,{a,c,d} from './module2.mjs';
+console.log(uio);
+console.log(a);
+console.log(c);
+console.log(d);
---
+++ src/Understanding export/module2.mjs
@@ -0,0 +1,9 @@
+const a = 'Harry';
+const b = "Prakash";
+const c = "Rohan";
+const d = "Hero";
+
+export default b;
+export {a};
+export {c};
+export {d};
